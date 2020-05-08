28a142a50e8b0568f9debd5836bfa1a84dbe1b1e
Vasileios Balafas <me@vasilis.pw>
2020-05-08 22:33:34 +0300

    Updates

--- OID/convert_annotations.py
+++ OID/convert_annotations.py
@@ -5,16 +5,17 @@ from tqdm import tqdm
 import argparse
 import fileinput
 
-# function that turns XMin, YMin, XMax, YMax coordinates to normalized YOLOv3 format
+
+# function that turns XMin, YMin, XMax, YMax coordinates to normalized yolo format
 def convert(filename_str, coords):
-    os.chdir("../../vasilis/thesis")
+    os.chdir("..")
     image = cv2.imread(filename_str + ".jpg")
     coords[2] -= coords[0]
     coords[3] -= coords[1]
-    x_diff = int(coords[2]/2)
-    y_diff = int(coords[3]/2)
-    coords[0] = coords[0]+x_diff
-    coords[1] = coords[1]+y_diff
+    x_diff = int(coords[2] / 2)
+    y_diff = int(coords[3] / 2)
+    coords[0] = coords[0] + x_diff
+    coords[1] = coords[1] + y_diff
     coords[0] /= int(image.shape[1])
     coords[1] /= int(image.shape[0])
     coords[2] /= int(image.shape[1])
@@ -22,9 +23,10 @@ def convert(filename_str, coords):
     os.chdir("Label")
     return coords
 
+
 ROOT_DIR = os.getcwd()
 
-# create dict to map class names to numbers for YOLOv3
+# create dict to map class names to numbers for yolo
 classes = {}
 with open("classes.txt", "r") as myFile:
     for num, line in enumerate(myFile, 0):
@@ -40,14 +42,14 @@ for DIR in DIRS:
     if os.path.isdir(DIR):
         os.chdir(DIR)
         print("Currently in subdirectory:", DIR)
-        
+
         CLASS_DIRS = os.listdir(os.getcwd())
         # for all class folders step into directory to change annotations
         for CLASS_DIR in CLASS_DIRS:
             if os.path.isdir(CLASS_DIR):
                 os.chdir(CLASS_DIR)
                 print("Converting annotations for class: ", CLASS_DIR)
-                
+
                 # Step into Label folder where annotations are generated
                 os.chdir("Label")
 
@@ -60,20 +62,22 @@ for DIR in DIRS:
                                 for class_type in classes:
                                     line = line.replace(class_type, str(classes.get(class_type)))
                                 labels = line.split()
-                                coords = np.asarray([float(labels[1]), float(labels[2]), float(labels[3]), float(labels[4])])
+                                coords = np.asarray(
+                                    [float(labels[1]), float(labels[2]), float(labels[3]), float(labels[4])])
                                 coords = convert(filename_str, coords)
                                 labels[1], labels[2], labels[3], labels[4] = coords[0], coords[1], coords[2], coords[3]
-                                newline = str(labels[0]) + " " + str(labels[1]) + " " + str(labels[2]) + " " + str(labels[3]) + " " + str(labels[4])
+                                newline = str(labels[0]) + " " + str(labels[1]) + " " + str(labels[2]) + " " + str(
+                                    labels[3]) + " " + str(labels[4])
                                 line = line.replace(line, newline)
                                 annotations.append(line)
                             f.close()
-                        os.chdir("../../vasilis/thesis")
+                        os.chdir("..")
                         with open(filename, "w") as outfile:
                             for line in annotations:
                                 outfile.write(line)
                                 outfile.write("\n")
                             outfile.close()
                         os.chdir("Label")
-                os.chdir("../../vasilis/thesis")
-                os.chdir("../../vasilis/thesis")
-        os.chdir("../../vasilis/thesis")
+                os.chdir("..")
+                os.chdir("..")
+        os.chdir("..")
